f8c68c6d2eae007786134db34c61fb80cc56dd2c
chastitycaptions-blog <hoppybitter@gmail.com>
2026-05-28 21:48:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let images = [];
+
+fetch("images.json")
+  .then(response => response.json())
+  .then(data => {
+    images = data;
+  });
+
+function spin() {
+  if (images.length === 0) return;
+
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const img = document.getElementById("image");
+  img.src = images[randomIndex];
+  img.style.display = "block";
+}
 
